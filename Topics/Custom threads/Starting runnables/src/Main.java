b7c4491bfecf446d035d8228d510da1b9687877d
zoranjuras <60330306+zoranjuras@users.noreply.github.com>
2024-09-25 14:11:43 +0200

    Delete Topics directory

--- Topics/Custom threads/Starting runnables/src/Main.java
+++
@@ -1,8 +0,0 @@
-class Starter {
-
-    public static void startRunnables(Runnable[] runnables) {
-        for (Runnable runnable : runnables) {
-            new Thread(runnable).start();
-        }
-    }
-}
--- Topics/Threads as objects/Is thread daemon/src/Main.java
+++
@@ -1,7 +0,0 @@
-class ThreadUtil {
-    public static void printIfDaemon(Thread thread) {
-        String message;
-        message = thread.isDaemon()? "daemon" : "not daemon";
-        System.out.println(message);
-    }
-}
